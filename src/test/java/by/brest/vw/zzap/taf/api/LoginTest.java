0dc373be84fd0e054d03779d18ea5a55e23b8bad
MirkoArtem <aikmir64@gmail.com>
2023-11-10 23:43:27 +0300

    add LoginTest

--- src/test/java/by/brest/vw/zzap/taf/api/LoginTest.java
+++ src/test/java/by/brest/vw/zzap/taf/api/LoginTest.java
@@ -1,21 +1,25 @@
 package by.brest.vw.zzap.taf.api;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginTest {
     @Test
-    public void loginWithNonExistingCredentials() {
-        String requestBody = "{\n" +
-                "    \"login\": \"test@test.com\",\n" +
-                "    \"password\": \"1q2w3e4r5t\"\n" +
-                "}";
-        given().body(requestBody).header("Content-Type", "application/json")
-                .when().post("")
-                .then().statusCode(400)
-                .assertThat().body("errors[0].key", equalTo("invalid_login_or_password"))
-                .body("errors[0].message", equalTo("Неверный логин или пароль"));
+    public void shouldSuccessfullyLogin() {
+        String response = given()
+                .contentType("multipart/form-data")
+                .multiPart("login", "aikmir64@gmail.com")
+                .multiPart("pass", "QAZwsx123")
+                .header("Connection", "keep-alive")
+                .header("Accept-Encoding", "gzip, deflate, br")
+                .header("Accept", "*/*")
+                .header("User-Agent", "PostmanRuntime/7.35.0")
+                .when()
+                .post("https://zzap.vw-brest.by/")
+                .then().statusCode(200)
+                .extract().body().asPrettyString();
+
+        assertTrue(response.contains("Мирко Артём"));
     }
 }
